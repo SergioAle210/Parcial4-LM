29c3e6685d5dd9597fcf6c15bc97617d085f8922
SergioAle210 <ore221122@uvg.edu.gt>
2024-11-11 16:58:05 -0600

    FEAT: Se modificaron las unit tests

--- test_turing_machine.py
+++ test_turing_machine.py
@@ -1,30 +1,21 @@
 import unittest
 from turing_machine import (
     TuringMachine,
+    load_machine_config,
 )  # Asegúrate de que tu código principal esté en un archivo llamado turing_machine.py
 
 
 class TestTuringMachine(unittest.TestCase):
 
     def setUp(self):
-        """Configura las transiciones y estados antes de cada prueba."""
-        self.transitions = {
-            ("q0", "a"): ("a", "R", "q1"),
-            ("q0", "b"): ("b", "R", "qloop"),
-            ("q0", "_"): ("_", "R", "qreject"),
-            ("q1", "a"): ("a", "R", "q1"),
-            ("q1", "b"): ("b", "R", "q2"),
-            ("q1", "_"): ("_", "R", "qreject"),
-            ("q2", "a"): ("a", "R", "qaccept"),
-            ("q2", "b"): ("b", "R", "qloop"),
-            ("q2", "_"): ("_", "R", "qloop"),
-            ("qloop", "a"): ("a", "R", "q1"),
-            ("qloop", "b"): ("b", "R", "qloop"),
-            ("qloop", "_"): ("_", "R", "qloop"),
-        }
-        self.initial_state = "q0"
-        self.accept_state = "qaccept"
-        self.reject_state = "qreject"
+        """Carga la configuración de la máquina de Turing desde el archivo JSON antes de cada prueba."""
+        (
+            self.initial_state,
+            self.accept_state,
+            self.reject_state,
+            self.blank_symbol,
+            self.transitions,
+        ) = load_machine_config("turing_config.json")
 
     def test_accept_input(self):
         """Prueba una cadena que debería ser aceptada."""
